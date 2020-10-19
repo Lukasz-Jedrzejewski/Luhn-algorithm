c327ff9cbc08e8690239b84f79c8d33475c9fd71
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-10-19 17:04:15 +0200

    first commit

---
+++ src/main/java/com/jedrzejewski/Luhn/algorithm/LuhnAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.jedrzejewski.Luhn.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuhnAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuhnAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jedrzejewski/Luhn/algorithm/LuhnAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedrzejewski.Luhn.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuhnAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
